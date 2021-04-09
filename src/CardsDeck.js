c2fceed7eb86a20001ce0ad78d898daccd9151ca
David Zemel <zemel96@gmail.com>
2021-04-09 19:18:39 -0400

    finished fs

--- src/CardsDeck.js
+++ src/CardsDeck.js
@@ -8,6 +8,8 @@ function CardsDeck() {
   const [cards, setCards] = useState([]);
   const [deck, setDeck] = useState({});
   const [isShuffling, setIsShuffling] = useState(false);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const timerId = useRef();
 
   useEffect(function () {
     async function getNewDeck() {
@@ -17,42 +19,84 @@ function CardsDeck() {
     getNewDeck();
   }, []);
 
-  async function pullCard() {
-    if(deck.remaining > 0) {
-      let card = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
-      setCards([...cards, card.data.cards[0]]);
-      setDeck({...deck, remaining:card.data.remaining})
-    }
-  }
+  useEffect(
+    function () {
+      async function pullCard() {
+        if (deck.remaining > 0) {
+          let card = await axios.get(
+            `${BASE_URL}/${deck.deck_id}/draw/?count=1`
+          );
+          console.log("card", card);
+          console.log("deck remaining", deck.remaining);
+          setCards((cards) => [...cards, card.data.cards[0]]);
+          setDeck((deck) => ({ ...deck, remaining: card.data.remaining }));
+        } else {
+          console.log("are we here?");
+          setIsDrawing(false);
+        }
+      }
+      async function drawEverySecond() {
+        console.log("EFFECT RAN!");
+        if (isDrawing) {
+          pullCard();
+        }
+      }
+      timerId.current = setInterval(() => {
+        drawEverySecond();
+        if (!isDrawing) {
+          clearInterval(timerId.current);
+        }
+      }, 1000);
+      return function cleanUpClearTimer() {
+        console.log("Unmount ID", timerId.current);
+        clearInterval(timerId.current);
+      };
+    },
+    [timerId, isDrawing, deck]
+  );
 
-  useEffect(function(){
-    async function shuffleDeck(){
-      setCards([]);
-      console.log("In shuffleDeck and isShuffling:", isShuffling);
-      let shuffledDeck = await axios.get(`${BASE_URL}/${deck.deck_id}/shuffle`);
-      setDeck(shuffledDeck.data);
-      setIsShuffling(false);
-    }
+  useEffect(
+    function () {
+      async function shuffleDeck() {
+        setCards([]);
+        console.log("In shuffleDeck and isShuffling:", isShuffling);
+        let shuffledDeck = await axios.get(
+          `${BASE_URL}/${deck.deck_id}/shuffle`
+        );
+        setDeck(shuffledDeck.data);
+        setIsShuffling(false);
+      }
 
-    if(isShuffling){
-      shuffleDeck();
-    }
+      if (isShuffling) {
+        shuffleDeck();
+      }
+    },
+    [isShuffling, deck.deck_id]
+  );
 
-  }, [isShuffling, deck.deck_id]);
-
-  function toggleShuffle(){
+  function toggleShuffle() {
     setIsShuffling(true);
   }
 
-  console.log(deck);
+  function toggleDraw() {
+    setIsDrawing((oldState) => !oldState);
+  }
 
   return (
     <div>
-      {deck.remaining === 0 ? <div>No cards remaining!</div> : <button onClick={pullCard}>Gimme a card!</button>}
+      {deck.remaining === 0 ? (
+        <div>No cards remaining!</div>
+      ) : (
+        <button onClick={toggleDraw}>Gimme a card!</button>
+      )}
       <div>
-        {cards.map(card => <Card key={card.code} card={card}/>)}
+        {cards.map((card) => {
+          return <Card key={card.code} card={card} />;
+        })}
       </div>
-      {isShuffling ?  <button disabled>Shuffle</button> : <button onClick={toggleShuffle}>Shuffle</button>}
+      <button onClick={toggleShuffle} disabled={isShuffling}>
+        Shuffle
+      </button>
     </div>
   );
 }
